7476af6064f8aae917a7b2ae7709ee091838d823
DucAnhPhi <ducanhphi1@gmail.com>
2019-04-01 18:30:44 +0200

    add palindrome checking

---
+++ transform.py
@@ -0,0 +1,23 @@
+class Transform(object):
+    def palindrome(self, n):
+        if self.is_palindrome(n):
+            return n
+        
+        tempN = n
+
+        while tempN < 1000000000:
+            tempN = tempN + self.reverse(tempN)
+            if self.is_palindrome(tempN):
+                return tempN
+
+        return -1
+    
+    def reverse(self, n):
+        rev = 0
+        while n > 0:
+            rev = (10 * rev) + n % 10
+            n //= 10
+        return rev
+    
+    def is_palindrome(self, n):
+        return n == self.reverse(n)
