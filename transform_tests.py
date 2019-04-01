78ba5f9bf1f1b852d19f1cc785cdab50759339d0
DucAnhPhi <ducanhphi1@gmail.com>
2019-04-01 18:47:45 +0200

    add unit tests for palindrome checking

---
+++ transform_tests.py
@@ -0,0 +1,23 @@
+import unittest
+from transform import Transform
+
+class TestTransformMethods(unittest.TestCase):
+    t = Transform()
+
+    def test_is_palindrome(self):
+        self.assertEqual(self.t.is_palindrome(121), True)
+        self.assertEqual(self.t.is_palindrome(11111),True)
+        self.assertEqual(self.t.is_palindrome(34543), True)
+        self.assertEqual(self.t.is_palindrome(3443), True)
+        self.assertEqual(self.t.is_palindrome(3434), False)
+        self.assertEqual(self.t.is_palindrome(1000), False)
+
+    def test_palindrome(self):
+        self.assertEqual(self.t.palindrome(28), 121)
+        self.assertEqual(self.t.palindrome(51), 66)
+        self.assertEqual(self.t.palindrome(11), 11)
+        self.assertEqual(self.t.palindrome(607), 4444)
+        self.assertEqual(self.t.palindrome(196), -1)
+
+if __name__ == '__main__':
+    unittest.main()
